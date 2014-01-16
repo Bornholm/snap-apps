2ae289dc2f33bb1dc4f53d3283d887943945d36e
William PETIT <william.petit@ac-dijon.fr>
2014-01-16 17:03:29 +0100

    Update Home app to new sandbox model

--- home/scripts/home.js
+++ home/scripts/home.js
@@ -1,86 +1,94 @@
 (function(w) {
 
   'use strict';
-  var document = w.document;
-  var panel = document.getElementById('panel-1');
 
   var DEFAULT_ICON = 'icons/default.png';
 
-  w.startApp = function(sandbox) {
+  var Snap = w.Snap;
+  var document = w.document;
+  var panel = document.getElementById('panel-1');
 
-    var currentApp = sandbox.apps.getCurrentApp();
+  if(Snap) {
 
-    sandbox.apps.getAppsList(function(err, apps) {
+    // Get Snap sandbox
+    Snap.ready(function(err, sandbox) {
 
-      if(err) {
-        // TODO dispatch error
+      // Fest apps list
+      sandbox.apps.getAppsList(function(err, apps) {
+        if(err) {
+          throw err; // TODO dispatch error
+        }
+        // For each app available, except home, create an icon
+        apps.forEach(function(appName) {
+          if(appName !== 'home') {
+            var appItem = createAppItem(appName);
+            panel.appendChild(appItem);
+          }
+        });
+      });
+
+      function setAppIcon(appName, iconEl) {
+        var marker = Date.now().toString(16);
+        getAppIconURL(appName, function(err, iconURL) {
+          if(err) {
+            throw err;
+          }
+          iconEl.style.backgroundImage = 'url(' + iconURL + ')';
+        });
+      };
+
+      function getAppIconURL(appName, cb) {
+        sandbox.apps.getAppManifest(appName, function(err, manifest) {
+          if(err) {
+            return cb(err);
+          }
+          if(manifest && manifest.icons) {
+            var sizes = Object.keys(manifest.icons);
+            var max = sizes.reduce(getMaxSize, 0);
+            if(max > 0) {
+              var iconURL = '/apps/' + appName + '/' + manifest.icons[''+max];
+              return cb(null, iconURL);
+            }
+          }
+          return cb(null, DEFAULT_ICON);
+        });
       }
 
-      apps.forEach(function(appName) {
-        if(currentApp !== appName) {
-          var appItem = createAppItem(appName);
-          panel.appendChild(appItem);
-        }
-      });
+      function getMaxSize(prev, curr) {
+        return Math.max(+prev, +curr);
+      }
+
+      function loadApp(appName) {
+        return sandbox.apps.loadApp(appName);
+      }
+
+      var COLORS = ['lime', 'blue', 'canary', 'rose'];
+      var colorIndex = 3;
+
+      function createAppItem(appName) {
+
+        var appItem = document.createElement('div');
+        appItem.className = 'app';
+
+        var appIcon = document.createElement('div');
+        appIcon.className = 'icon ' + COLORS[(colorIndex++)%COLORS.length];
+        setAppIcon(appName, appIcon);
+        appItem.appendChild(appIcon);
+
+        var appLabel = document.createElement('span');
+        appLabel.className = 'label';
+        appLabel.innerHTML = appName;
+        appItem.appendChild(appLabel);
+
+        appItem.onclick = loadApp.bind(null, appName);
+
+        return appItem;
+      }
 
     });
 
-    var COLORS = ['lime', 'blue', 'canary', 'rose'];
-    var colorIndex = 3;
-    function createAppItem(appName) {
+  } else {
+    throw new Error('Not in a Snap context !');
+  }
 
-      var appItem = document.createElement('div');
-      appItem.className = 'app';
-
-      var appIcon = document.createElement('div');
-      appIcon.className = 'icon ' + COLORS[(colorIndex++)%COLORS.length];
-      appItem.appendChild(appIcon);
-
-      var appLabel = document.createElement('span');
-      appLabel.className = 'label';
-      appLabel.innerHTML = appName;
-      appItem.appendChild(appLabel);
-
-      getAppIconURL(appName, function(err, iconURL) {
-        if(err) {
-          // TODO dispatch error
-        }
-        appIcon.style.backgroundImage = 'url(' + iconURL + ')';
-      });
-
-      appItem.onclick = loadApp.bind(null, appName);
-
-      return appItem;
-    }
-
-    function getAppIconURL(appName, cb) {
-      sandbox.apps.getAppManifest(appName, function(err, manifest) {
-        if(err) {
-          return cb(err);
-        }
-        if(manifest && manifest.icons) {
-          var sizes = Object.keys(manifest.icons);
-          var max = sizes.reduce(getMaxSize, 0);
-          if(max > 0) {
-            var iconURL = '/apps/' + appName + '/' + manifest.icons[''+max];
-            return cb(null, iconURL);
-          } else {
-            return cb(null, DEFAULT_ICON);
-          }
-        } else {
-          return cb(null, DEFAULT_ICON);
-        }
-      });
-    }
-
-    function getMaxSize(prev, curr) {
-      return Math.max(+prev, +curr);
-    }
-
-    function loadApp(appName) {
-      return sandbox.apps.loadApp(appName);
-    };
-
-  };
-
-}(window))
+}(window));
