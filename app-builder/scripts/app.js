edf932131ee8da00b7decec0510af85e92304353
Will <william.petit@lookingfora.name>
2014-04-14 20:53:51 +0200

    Basic editor with drag & drop

--- app-builder/scripts/app.js
+++ app-builder/scripts/app.js
@@ -1,35 +1,54 @@
 (function() {
 
-  var workspace = document.getElementById('workspace');
-  var widgets = document.getElementById('widgets');
+  var $workspace = $('#workspace');
+  var $widgets = $('#widgets');
 
   function dragHandler(evt) {
     var el = evt.target;
-    evt.dataTransfer.effectAllowed = 'move';
-    evt.dataTransfer.setData('text/plain', el.dataset.widgetType);
+    var dataTransfer = evt.originalEvent.dataTransfer;
+    dataTransfer.effectAllowed = 'move';
+    dataTransfer.setData('text/plain', el.dataset.widgetType);
   }
 
   function dragOverHandler(evt) {
-    evt.preventDefault();
     return false;
   }
 
   function dropHandler(evt) {
-    var widgetType = evt.dataTransfer.getData('text/plain');
+    var dataTransfer = evt.originalEvent.dataTransfer;
+    var widgetType = dataTransfer.getData('text/plain');
     if(widgetType) {
       var isContainer = evt.target.tagName === 'SNAP-CONTAINER';
       addWidget(widgetType, isContainer ? evt.target : evt.target.parentNode);
     }
+    return false;
   }
 
   function addWidget(tagName, target) {
-    target = target || workspace;
+    var $target = $(target || workspace);
     var widget = document.createElement(tagName);
-    target.appendChild(widget);
-  };
+    widget.style.width = 0;
+    $target.append(widget);
+  }
 
-  widgets.addEventListener('dragstart', dragHandler, false);
-  workspace.addEventListener('dragover', dragOverHandler, false);
-  workspace.addEventListener('drop', dropHandler, false);
+  function removeHandler(evt) {
+    $(evt.target).remove();
+  }
+
+  $widgets.bind('dragstart', dragHandler);
+  $workspace.bind({
+    'dragover': dragOverHandler,
+    'drop': dropHandler,
+    'removeclick': removeHandler
+  });
+
+  $('#toggleView').bind('click', function() {
+    var viewOnly = $workspace.attr('view-only');
+    if(viewOnly) {
+      $workspace.removeAttr('view-only');
+    } else {
+      $workspace.attr('view-only', true);
+    }
+  });
 
 }());
