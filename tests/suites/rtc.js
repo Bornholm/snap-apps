80a3923fe4c5906d5760e6e893dae1e09ec2fb49
William Petit <william.petit@ptitcloud.fr>
2015-01-01 12:48:43 +0100

    Miseà jour tests RTC

--- tests/suites/rtc.js
+++ tests/suites/rtc.js
@@ -1,13 +1,13 @@
-this.messages =  {
+this.rtc =  {
 
   setUp: function(cb) {
     this.sandbox = window.sandbox;
-    window.Snap.on('message', console.log.bind(console));
+    this.sandbox.rtc.on('rtc:message', console.log.bind(console));
     return cb();
   },
 
   broadcast: function(test) {
-    this.sandbox.messages.broadcast({foo: 'bar'}, function(err, user) {
+    this.sandbox.rtc.broadcast({foo: 'bar'}, function(err, user) {
       test.ifError(err);
       test.done();
     });
